891018aee131367b4f6dbb7f2ed3578b8b4b1972
Christopher Li <Christopherfooli@gmail.com>
2020-04-09 04:56:45 -0700

    Add basic typescript building block types

---
+++ src/server/game/entities/basic_types.ts
@@ -0,0 +1,69 @@
+class Point {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	transform(velocity: Velocity): Point {
+		return new Point(this.x + velocity.x, this.y + velocity.y);
+	}
+}
+
+class Velocity {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	getSpeed(): number {
+		return Math.sqrt(this.x^2 + this.y^2);
+	}
+}
+
+class Rectangle {
+	// This x, y point marks the top left corner of the rectangle when angle = 0;
+	point: Point;
+	width: number;
+	height: number;
+	// Angle is measured in 0 - 360 degrees, with 0 being the neutral state where
+	// the 4 vertices of the rectangle are:
+	// [(x, y), (x + width, y), (x + width, y - height), (x, y - height)]
+	// in clockwise order. An angle of 90 would rotate the rectangle 90 degrees
+	// clockwise, so that new verticies would be:
+	// [(x, y), (x, y - width), (x - height, y - width), (x - height, y)]
+	angle: number;
+
+	constructor(x: number, y:number, width: number, height: number, angle = 0) {
+		this.point = new Point(x, y);
+		this.width = width;
+		this.height = height;
+		this.angle = angle;
+	}
+
+	isCollision(rectangle: Rectangle): boolean {
+		if (this.angle != 0 || rectangle.angle != 0) {
+			throw 'Unimplemented';
+		}
+
+		return this.point.x <= rectangle.x + rectangle.width
+			&& rectangle.x <= this.point.x + this.width
+			&& this.point.y <= rectangle.y + rectangle.height
+			&& rectangle.y <= this.point.y + this.height;
+	}
+
+	transform(velocity: Velocity): Rectangle {
+		return new (this.x + velocity.x, this.y + velocity.y,
+			this.width, this.height, this.angle);
+	}
+}
+
+// TODO Circle
+// integrate circle collisions with rectangles
+
+export { Point, Velocity, Rectangle }

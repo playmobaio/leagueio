a975acadb3d3a6e3209b30389484480ef94c8e4e
jiham7 <jiham7@gmail.com>
2020-04-29 22:10:14 -0500

    edit tests in registerAutoAttack

--- tests/server/player_tests.ts
+++ tests/server/player_tests.ts
@@ -22,6 +22,7 @@ describe('Player', function() {
     game.projectiles.clear();
     point = new Point(0, 1);
     player = new Player(id, new Point(0, 0), socket.object);
+    player.lastAutoAttackFrame = -1;
   });
 
   describe('#update', function() {
@@ -40,19 +41,22 @@ describe('Player', function() {
 
   describe("#addProjectile", function() {
     it("Smoke", function() {
+      game.projectiles.clear();
       const projectile: Projectile = game.addProjectile(player.id, point);
-      assert.equal(constants.DEFAULT_PROJECTILE_TO_USER_OFFSET, projectile.position.y);
+      assert.equal(constants.DEFAULT_PROJECTILE_TO_USER_OFFSET, projectile.model.center.y);
       assert.equal(constants.DEFAULT_PROJECTILE_SPEED, projectile.velocity.getSpeed());
     });
   });
 
   describe("#registerAutoAttack", function() {
     it("will autoattack immediately on spawn", function() {
+      game.projectiles.clear();
       player.registerAutoAttack(point);
       assert.equal(game.projectiles.size, 1);
     });
 
     it("cannot auto attack too soon", function() {
+      game.projectiles.clear();
       player.lastAutoAttackFrame = 0;
       player.attackSpeed = 1;
       game.currentFrame = 59; // Should be able to attack after 1 second, or 60 frams at 60 fps
@@ -62,6 +66,7 @@ describe('Player', function() {
     });
 
     it("can auto attack after set number of frames", function() {
+      game.projectiles.clear();
       player.lastAutoAttackFrame = 0;
       player.attackSpeed = 1;
       game.currentFrame = 60; // Should be able to attack after 1 second, or 60 frams at 60 fps

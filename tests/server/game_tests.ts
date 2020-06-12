13eef15fdee73a3fa9db0a408f7063afe2b94e4f
Christopher Li <Christopherfooli@gmail.com>
2020-06-11 18:50:08 -0700

    fix tests

--- tests/server/game_tests.ts
+++ tests/server/game_tests.ts
@@ -4,6 +4,8 @@ import Player from '../../src/server/models/player';
 
 describe('Game', function() {
   let game: Game;
+  const defaultName = "DEFAULT_NAME";
+  const defaultHeroID = 1;
   const id = "id";
 
   beforeEach(function(){
@@ -12,7 +14,7 @@ describe('Game', function() {
 
   describe('#AddandRemovePlayers', function() {
     it('Game can add and remove players', function() {
-      Player.create(id, null);
+      Player.create(id, null, defaultName, defaultHeroID);
       assert.equal(1, game.players.size);
       game.removePlayer(id);
       assert.equal(0, game.players.size);

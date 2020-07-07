cd0d52dfc8293cfa37e20b0e3c4778e16903d4c1
Philip Lan <philipslan@github.com>
2020-07-06 20:43:44 -0400

    adding phaser

--- src/client/js/game.ts
+++ src/client/js/game.ts
@@ -7,6 +7,43 @@ import { drawClientStocks } from './draw/stocks';
 import { drawClientHud } from './draw/hud';
 import UserInputController from './userInputController';
 import drawHeroState from './draw/heroState';
+import 'phaser';
+
+class Demo extends Phaser.Scene
+{
+  constructor()
+  {
+    super('demo');
+  }
+
+  preload(): void
+  {
+    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image('libs', 'assets/libs.png');
+    this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
+    this.load.glsl('stars', 'assets/starfields.glsl.js');
+  }
+
+  create(): void
+  {
+    this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+
+    this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+
+    this.add.image(400, 300, 'libs');
+
+    const logo = this.add.image(400, 70, 'logo');
+
+    this.tweens.add({
+      targets: logo,
+      y: 350,
+      duration: 1500,
+      ease: 'Sine.inOut',
+      yoyo: true,
+      repeat: -1
+    })
+  }
+}
 
 // Client
 class Game {
@@ -30,6 +67,14 @@ class Game {
       const camera = new Camera(gameMap, gameMap.canvas.width, gameMap.canvas.height);
       const game = new Game(gameMap, camera);
       Game.instance = game;
+      const config = {
+        type: Phaser.AUTO,
+        backgroundColor: '#125555',
+        width: 800,
+        height: 600,
+        scene: Demo
+      };
+      new Phaser.Game(config);
     }
     return Game.instance;
   }

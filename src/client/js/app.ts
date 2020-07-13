270d1b8ffc086676a872eecaea4f1547cdde8d16
jiham7 <jiham7@gmail.com>
2020-07-13 13:05:43 -0500

    add phaser option (#132)
    * add phaser option
    
    Co-authored-by: Philip Lan <philipslan@gmail.com>

--- src/client/js/app.ts
+++ src/client/js/app.ts
@@ -4,6 +4,10 @@ import { registerSocket } from './socket';
 import Layers from './layer';
 import Game from './game';
 
+import 'phaser';
+import GameScene from './scenes/gameScene';
+import UIScene from './scenes/uiScene';
+
 function resizeCanvas(): void {
   const canvas: HTMLElement = document.getElementById("canvas");
   const width: number = window.innerWidth;
@@ -13,31 +17,63 @@ function resizeCanvas(): void {
   canvas.style.height = height + "px";
 }
 
-function InitializeGameUI(): void {
+function InitializeGameUI(fullScreen: boolean): void {
   document.getElementsByTagName("body")[0].style.margin = "0px 0px 0px 0px";
   document.getElementById("startpanel").style.visibility = "hidden";
   document.getElementById("startpanel").style.height = "0px";
   document.getElementById("game").style.visibility = "visible";
-  document.getElementById("game").requestFullscreen();
+  if (fullScreen) {
+    document.getElementById("game").requestFullscreen();
+  }
   resizeCanvas();
 }
 
-document.getElementById("joinGame").onclick = (): void => {
-  if (Layers.getLayers() == undefined) {
-    alert("Loading assets. Please try again");
-    return;
+function InitializePhaserUI(fullScreen: boolean): void {
+  document.getElementById('startpanel').setAttribute("style", "display:none;");
+  document.getElementById('game').setAttribute("style", "display:none;");
+  const config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 576,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    backgroundColor: '#89CFF0',
+    scene: [ GameScene, UIScene ]
+  };
+  const game = new Phaser.Game(config);
+  if (fullScreen) {
+    game.scale.startFullscreen();
   }
+}
+
+function InitializeSocket(): void {
   console.log("Initializing Socket");
+  const socket: SocketIO.Socket = io();
   const name: string = (document.getElementById("playerName") as HTMLInputElement).value;
   const heroId: HeroID = parseInt((document.getElementById("hero") as HTMLInputElement).value);
   const joinGame: IJoinGame = { name, heroId };
-  Game.getInstance().heroId = heroId;
-
-  const socket: SocketIO.Socket = io();
   registerSocket(socket);
-  InitializeGameUI();
-  window.addEventListener('resize', resizeCanvas);
   socket.emit("C:JOIN_GAME", joinGame);
+  Game.getInstance().heroId = heroId;
+}
+
+document.getElementById("joinGame").onclick = (): void => {
+  const usePhaser: boolean = (document.getElementById("usePhaser") as HTMLInputElement).checked;
+  const fullScreen: boolean = (document.getElementById("fullScreen") as HTMLInputElement).checked;
+  if (!usePhaser && Layers.getLayers() == undefined) {
+    alert("Loading assets. Please try again");
+    return;
+  }
+  InitializeSocket();
+  if(usePhaser) {
+    InitializePhaserUI(fullScreen);
+  }
+  else {
+    InitializeGameUI(fullScreen);
+    window.addEventListener('resize', resizeCanvas);
+  }
 };
 
 Layers.createAsync();

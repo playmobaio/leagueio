71fe16b66c8bbae7b68b2884b3c29dc2cb519dd7
jiham7 <jiham7@gmail.com>
2020-08-03 16:08:57 -0500

    added hud for gametime / score

--- src/client/js/scenes/hudScene.ts
+++ src/client/js/scenes/hudScene.ts
@@ -2,10 +2,12 @@ import 'phaser';
 import { IGameState } from '../../../models/interfaces';
 import PhaserInputController from '../phaserInputController';
 import { drawStocks } from './draw/stocks';
+import { drawGameTime } from './draw/gameTime';
 
 class HudScene extends Phaser.Scene {
   socket: SocketIO.Socket;
   stockText: Phaser.GameObjects.Text;
+  gameTimeText: Phaser.GameObjects.Text;
 
   constructor()
   {
@@ -25,6 +27,7 @@ class HudScene extends Phaser.Scene {
 
   render(userGame: IGameState): void {
     drawStocks(this, userGame.client);
+    drawGameTime(this, userGame.currentFrame);
   }
 }
 export default HudScene;

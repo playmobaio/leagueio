d7e84d72fa1b4d8e5a56a2d071eb9b3ddf249ec0
Philip Lan <philipslan@gmail.com>
2020-08-24 21:12:55 -0400

    adding flash and ghost abilities (#171)

---
+++ src/server/hero/abilities/ghost.ts
@@ -0,0 +1,18 @@
+import Ability from '../ability';
+import modelConstants from '../../../models/constants';
+import { CastRestriction } from '../../../models/interfaces';
+import MovementSpeedModifierEffect from '../effects/movementSpeedModifierEffect';
+
+class Ghost extends Ability {
+  cooldown = 30;
+  name = modelConstants.GHOST;
+  castRestriction = CastRestriction.None;
+
+  onCast(): void {
+    console.log("Casting Ghost");
+    const effect = new MovementSpeedModifierEffect(this.hero, 3, 1.5);
+    this.hero.state.addEffect(effect);
+  }
+}
+
+export default Ghost;

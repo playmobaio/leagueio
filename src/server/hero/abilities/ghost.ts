e8a8a91feba2395f25a8a5f7b18d44386da75d80
Christopher Li <Christopherfooli@gmail.com>
2020-09-21 19:14:09 -0700

    remove ghost flash cast time

--- src/server/hero/abilities/ghost.ts
+++ src/server/hero/abilities/ghost.ts
@@ -2,11 +2,17 @@ import Ability from '../ability';
 import modelConstants from '../../../models/constants';
 import { CastRestriction } from '../../../models/interfaces/iAbility';
 import MovementSpeedModifierEffect from '../effects/movementSpeedModifierEffect';
+import Hero from '../hero';
 
 class Ghost extends Ability {
   cooldown = 30;
   name = modelConstants.GHOST;
   castRestriction = CastRestriction.None;
+
+  constructor(hero: Hero) {
+    super(hero);
+    this.castTime = 0;
+  }
 
   onCast(): void {
     console.log("Casting Ghost");

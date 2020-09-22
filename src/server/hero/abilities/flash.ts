5ad5b7a1743a193e4aa08f658e6e17faf6d75800
Christopher-Li <Christopher-Li@users.noreply.github.com>
2020-09-21 19:31:52 -0700

    remove ghost flash cast time (#238)

--- src/server/hero/abilities/flash.ts
+++ src/server/hero/abilities/flash.ts
@@ -2,12 +2,18 @@ import Ability from '../ability';
 import modelConstants from '../../../models/constants';
 import { CastRestriction } from '../../../models/interfaces/iAbility';
 import { VectorBuilder } from '../../models/basicTypes';
+import Hero from '../hero';
 
 class Flash extends Ability {
   static range = 100;
   cooldown = 60;
   name = modelConstants.FLASH;
   castRestriction = CastRestriction.None;
+
+  constructor(hero: Hero) {
+    super(hero);
+    this.castTime = 0;
+  }
 
   onCast(): void {
     console.log("Casting Flash");

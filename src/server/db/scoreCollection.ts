70e79ef07c0f6d995250c34503ac45852d2fa6db
Christopher Li <Christopherfooli@gmail.com>
2020-10-12 22:10:38 -0700

    Increment a score counter so we know how many users have finished playing our game

--- src/server/db/scoreCollection.ts
+++ src/server/db/scoreCollection.ts
@@ -1,19 +1,29 @@
 import FirestoreBase from './firestoreBase';
 import { IScore } from '../models/iScore';
-import { CollectionReference, DocumentData, QuerySnapshot } from '@google-cloud/firestore';
+import { CollectionReference, DocumentData, QuerySnapshot, FieldValue } from '@google-cloud/firestore';
 
 class ScoreCollection extends FirestoreBase {
-  static collection = "scores";
+  static aggregationCollectionName = "aggregation";
+  static scoreCollectionName = "scores";
   scoreCollection: CollectionReference<DocumentData>;
+  aggregationCollection: CollectionReference<DocumentData>;
 
   constructor() {
     super();
-    this.scoreCollection = this.db.collection(ScoreCollection.collection);
+    this.scoreCollection = this.db.collection(ScoreCollection.scoreCollectionName);
+    this.aggregationCollection = this.db.collection(ScoreCollection.aggregationCollectionName);
   }
 
   addScore(score: IScore): void {
     this.scoreCollection.add(score);
+    this.incrementScoreCount();
   }
+
+  incrementScoreCount(): void {
+    const ref = this.aggregationCollection.doc(ScoreCollection.scoreCollectionName);
+    ref.update('total', FieldValue.increment(1));
+  }
+
 
   async getTopN(n: number): Promise<IScore[]> {
     const scoreQuery: QuerySnapshot<DocumentData> = await this.scoreCollection
